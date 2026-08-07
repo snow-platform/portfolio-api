f274951589d486e255f90a949e8afecdd63c1296
estellise-yukihime <shiranoyukihime@gmail.com>
2026-08-07 10:55:59 +0800

    fix: correct CMS endpoint query strings for consistent `populate` structure

--- PortfolioApi/ExternalServices/CMS/Content/GetReview.cs
+++ PortfolioApi/ExternalServices/CMS/Content/GetReview.cs
@@ -14,7 +14,7 @@ public class GetReview : ICmsContent
 
     public string Endpoint
     {
-        get => $"/api/reviews/{Uri.EscapeDataString(_slug)}?populate=cover&populate=author,author.avatar&populate=category&populate=tags&populate=blocks,blocks.file,blocks.files";
+        get => $"/api/reviews/{Uri.EscapeDataString(_slug)}?populate=cover&populate=author&populate=author.avatar&populate=category&populate=tags&populate=blocks&populate=blocks.file&populate=blocks.files";
     }
 
     public HttpMethod HttpMethod
