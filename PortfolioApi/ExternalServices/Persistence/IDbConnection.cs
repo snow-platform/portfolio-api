9479e0256f51c75a14c86d32e25d6ab2cde3cd0b
estellise-yukihime <shiranoyukihime@gmail.com>
2026-09-19 12:57:05 +0800

    feat: add asynchronous connection method to `IDbConnection` interface

--- PortfolioApi/ExternalServices/Persistence/IDbConnection.cs
+++ PortfolioApi/ExternalServices/Persistence/IDbConnection.cs
@@ -1,6 +1,7 @@
 namespace PortfolioApi.ExternalServices.Persistence;
 
-public interface IDbConnection<out T>
+public interface IDbConnection<T>
 {
     T OpenConnection();
+    ValueTask<T> OpenConnectionAsync();
 }
